561cdbe3e23bdbbcf49f7048ee428588bc948e7d
Ted <andrei.teodor21@yahoo.ro>
2022-09-09 11:54:57 +0200

    Created ASP.NET Core MVC project

---
+++ Semester3Project/Semester3Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Semester3Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}

3cfebf138933cfd4b1279892988ceb025e6f9933
Ted <andrei.teodor21@yahoo.ro>
2022-09-17 16:51:09 +0200

    Renamed a few files and folders

---
+++ QuizBytesSolution/QuizBytesLibrary/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBytesLibrary
+{
+    public class Course
+    {
+        public int MyProperty { get; set; }
+    }
+}

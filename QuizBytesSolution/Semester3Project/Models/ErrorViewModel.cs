7e9c3158f073db9c541baf16cdd2c4e37fe8c612
Ted <andrei.teodor21@yahoo.ro>
2022-09-21 21:23:25 +0200

    removed unnecessary folders from project

--- QuizBytesSolution/Semester3Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Semester3Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

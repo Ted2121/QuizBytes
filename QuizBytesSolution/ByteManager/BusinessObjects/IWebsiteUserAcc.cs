f2d2d95ba805be7d0c9878b877143ebc946aa418
kir40lmao <93580277+kir40lmao@users.noreply.github.com>
2022-09-21 12:44:36 +0200

    Added interfaces for classes that manipulate the DB

---
+++ QuizBytesSolution/ByteManager/BusinessObjects/IWebsiteUserAcc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteManager.BusinessObjects
+{
+    internal interface IWebsiteUserAcc
+    {
+        string Name { get; set; }
+        string password { get; set; }
+        string Email { get; set; }
+        int totalPoints { get; set; }
+    }
+}

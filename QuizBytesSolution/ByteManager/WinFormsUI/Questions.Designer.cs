a034c74145e7fe211732fe46eba418ee50b1e60a
Ted <andrei.teodor21@yahoo.ro>
2022-09-23 10:23:27 +0200

    Made a few changes to the data grid in Questions form

--- QuizBytesSolution/ByteManager/WinFormsUI/Questions.Designer.cs
+++ QuizBytesSolution/ByteManager/WinFormsUI/Questions.Designer.cs
@@ -41,13 +41,9 @@
             this.singleAnswerTabPage = new System.Windows.Forms.TabPage();
             this.singleAnswerDataGrid = new System.Windows.Forms.DataGridView();
             this.trueFalseTabPage = new System.Windows.Forms.TabPage();
+            this.questionsIndex = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.chapterName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.questionText = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.hint = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.correctAnswer = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.wrongAnswer1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.wrongAnswer2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.wrongAnswer3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.questionTabControl.SuspendLayout();
             this.singleAnswerTabPage.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.singleAnswerDataGrid)).BeginInit();
@@ -135,10 +131,10 @@
             // singleAnswerTabPage
             // 
             this.singleAnswerTabPage.Controls.Add(this.singleAnswerDataGrid);
-            this.singleAnswerTabPage.Location = new System.Drawing.Point(4, 39);
+            this.singleAnswerTabPage.Location = new System.Drawing.Point(4, 45);
             this.singleAnswerTabPage.Name = "singleAnswerTabPage";
             this.singleAnswerTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.singleAnswerTabPage.Size = new System.Drawing.Size(1407, 275);
+            this.singleAnswerTabPage.Size = new System.Drawing.Size(1407, 269);
             this.singleAnswerTabPage.TabIndex = 0;
             this.singleAnswerTabPage.Text = "Single Answer";
             this.singleAnswerTabPage.UseVisualStyleBackColor = true;
@@ -157,13 +153,9 @@
             this.singleAnswerDataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.singleAnswerDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.singleAnswerDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.questionsIndex,
             this.chapterName,
-            this.questionText,
-            this.hint,
-            this.correctAnswer,
-            this.wrongAnswer1,
-            this.wrongAnswer2,
-            this.wrongAnswer3});
+            this.questionText});
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -177,6 +169,8 @@
             this.singleAnswerDataGrid.GridColor = System.Drawing.Color.White;
             this.singleAnswerDataGrid.Location = new System.Drawing.Point(3, 3);
             this.singleAnswerDataGrid.Name = "singleAnswerDataGrid";
+            this.singleAnswerDataGrid.RowHeadersVisible = false;
+            this.singleAnswerDataGrid.RowHeadersWidth = 51;
             dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
             dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
@@ -184,62 +178,46 @@
             dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
             this.singleAnswerDataGrid.RowsDefaultCellStyle = dataGridViewCellStyle3;
             this.singleAnswerDataGrid.RowTemplate.Height = 25;
-            this.singleAnswerDataGrid.Size = new System.Drawing.Size(1401, 269);
+            this.singleAnswerDataGrid.Size = new System.Drawing.Size(1401, 263);
             this.singleAnswerDataGrid.TabIndex = 0;
             this.singleAnswerDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // trueFalseTabPage
             // 
-            this.trueFalseTabPage.Location = new System.Drawing.Point(4, 24);
+            this.trueFalseTabPage.Location = new System.Drawing.Point(4, 29);
             this.trueFalseTabPage.Name = "trueFalseTabPage";
             this.trueFalseTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.trueFalseTabPage.Size = new System.Drawing.Size(1407, 290);
+            this.trueFalseTabPage.Size = new System.Drawing.Size(1407, 285);
             this.trueFalseTabPage.TabIndex = 1;
             this.trueFalseTabPage.Text = "True/False Answer";
             this.trueFalseTabPage.UseVisualStyleBackColor = true;
             // 
+            // questionsIndex
+            // 
+            this.questionsIndex.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.questionsIndex.HeaderText = "No.";
+            this.questionsIndex.MinimumWidth = 81;
+            this.questionsIndex.Name = "questionsIndex";
+            this.questionsIndex.Width = 81;
+            // 
             // chapterName
             // 
-            this.chapterName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.chapterName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
             this.chapterName.HeaderText = "Chapter Name";
+            this.chapterName.MinimumWidth = 200;
             this.chapterName.Name = "chapterName";
-            this.chapterName.Width = 162;
+            this.chapterName.Width = 300;
             // 
             // questionText
             // 
             this.questionText.HeaderText = "Question Text";
-            this.questionText.MinimumWidth = 500;
+            this.questionText.MinimumWidth = 1015;
             this.questionText.Name = "questionText";
-            this.questionText.Width = 500;
-            // 
-            // hint
-            // 
-            this.hint.HeaderText = "Hint";
-            this.hint.Name = "hint";
-            // 
-            // correctAnswer
-            // 
-            this.correctAnswer.HeaderText = "Correct Answer";
-            this.correctAnswer.Name = "correctAnswer";
-            // 
-            // wrongAnswer1
-            // 
-            this.wrongAnswer1.HeaderText = "Wrong Answer 1";
-            this.wrongAnswer1.Name = "wrongAnswer1";
-            // 
-            // wrongAnswer2
-            // 
-            this.wrongAnswer2.HeaderText = "Wrong Answer 2";
-            this.wrongAnswer2.Name = "wrongAnswer2";
-            // 
-            // wrongAnswer3
-            // 
-            this.wrongAnswer3.HeaderText = "Wrong Answer 3";
-            this.wrongAnswer3.Name = "wrongAnswer3";
+            this.questionText.Width = 1015;
             // 
             // Questions
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
@@ -278,12 +256,8 @@
         private TabPage singleAnswerTabPage;
         private TabPage trueFalseTabPage;
         private DataGridView singleAnswerDataGrid;
+        private DataGridViewTextBoxColumn questionsIndex;
         private DataGridViewTextBoxColumn chapterName;
         private DataGridViewTextBoxColumn questionText;
-        private DataGridViewTextBoxColumn hint;
-        private DataGridViewTextBoxColumn correctAnswer;
-        private DataGridViewTextBoxColumn wrongAnswer1;
-        private DataGridViewTextBoxColumn wrongAnswer2;
-        private DataGridViewTextBoxColumn wrongAnswer3;
     }
 }

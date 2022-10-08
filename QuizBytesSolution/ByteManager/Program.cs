dbdd5826f6e9606b769c83bee2f5e85d8468cf9e
Ted <andrei.teodor21@yahoo.ro>
2022-10-08 18:17:25 +0200

    Minor changes

--- QuizBytesSolution/ByteManager/Program.cs
+++ QuizBytesSolution/ByteManager/Program.cs
@@ -13,7 +13,7 @@ namespace ByteManager.WinFormsUI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Login());
+            Application.Run(new Chapters());
         }
     }
 }

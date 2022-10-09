5c3c0ddd8d6fb0366d410f2dbcab0e9b53a2682c
Ted <andrei.teodor21@yahoo.ro>
2022-10-09 13:04:42 +0200

    Implemented methods for the ChaptersController API

---
+++ QuizBytesSolution/QuizBytesAPIServer/Controllers/ChaptersController.cs
@@ -0,0 +1,99 @@
+using Microsoft.AspNetCore.Mvc;
+using QuizBytesAPIServer.Models;
+using System.Security.Principal;
+
+namespace QuizBytesAPIServer.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ChapterController : ControllerBase
+    {
+        public IChapterDataAccess ChapterDataAccess { get; set; }
+
+        public ChapterController(IChapterDataAccess chapterDataAccess)
+        {
+            ChapterDataAccess = chapterDataAccess;
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteChapterAsync(Chapter chapter)
+        {
+            if (chapter == null)
+            {
+                return NotFound();
+            }
+
+            await ChapterDataAccess.DeleteChapterAsync(chapter);
+            // TODO figure out how to check if deletion was successfull
+
+            return Ok(); 
+        }
+
+
+        [HttpGet]
+        [Route("{all}")]
+        public async Task<ActionResult<IEnumerable<Chapter>>> GetAllChaptersAsync()
+        {
+            var chapters = await ChapterDataAccess.GetAllChaptersAsync();
+
+            if (chapters == null)
+            {
+                return NotFound();
+            }
+            return Ok(chapters);
+        }
+
+        [HttpGet]
+        [Route("subject")]
+        public async Task<ActionResult<IEnumerable<Chapter>>> GetAllChaptersBySubjectAsync()
+        {
+            var chapters = await ChapterDataAccess.GetAllChaptersBySubjectAsync();
+
+            if (chapters == null)
+            {
+                return NotFound();
+            }
+            return Ok(chapters); 
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Chapter>> GetChapterByIdAsync(int chapterId)
+        {
+            Chapter chapter = await ChapterDataAccess.GetChapterByIdAsync(chapterId);
+            if (chapter == null)
+            {
+                return NotFound();
+            }
+            return Ok(chapter); 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Chapter>> InsertChapterAsync(Chapter chapter)
+        {
+            chapter = await ChapterDataAccess.InsertChapterAsync(chapter);
+
+            if (chapter == null)
+            {
+                return NotFound();
+            }
+            return Ok(chapter); 
+        } 
+
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateChapterAsync(Chapter chapter)
+        {
+
+            if (chapter == null)
+            {
+                return NotFound();
+            }
+
+            await ChapterDataAccess.UpdateChapterAsync(chapter);
+
+            return Ok();
+
+        }
+    }
+}

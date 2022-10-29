fbe08a1410b9162d79ecce17f76d3c44e258f323
Ted <andrei.teodor21@yahoo.ro>
2022-10-29 18:41:58 +0200

    Added a ChallengeController

---
+++ QuizBytesSolution/QuizBytesWebsite/Controllers/ChallengeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizBytesWebsite.Controllers
+{
+    public class ChallengeController : Controller
+    {
+        public IQuizBytesApiClient _client { get; set; }
+
+        public ChallengeController(IQuizBytesApiClient client)
+        {
+            _client = client;
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+    }
+}

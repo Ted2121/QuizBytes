4a8854982983cb172ea68f0e956d8e1538efb40e
VladdyOnGitHub <vladimiryazadjiev@gmail.com>
2022-11-04 15:21:55 +0100

    Resize Solved for Questions Form

--- QuizBytesSolution/ByteManager/WinFormsUI/Questions.cs
+++ QuizBytesSolution/ByteManager/WinFormsUI/Questions.cs
@@ -16,40 +16,5 @@ namespace ByteManager.WinFormsUI
         {
             InitializeComponent();
         }
-
-        private void tabPage1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void questionTypeLabel_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void questionDeleteButton_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }

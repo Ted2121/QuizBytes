290fc29a5c37306d174e19a78ab436057e578d3e
kir40lmao <93580277+kir40lmao@users.noreply.github.com>
2022-11-13 22:28:11 +0100

    Failed Button logic
    I hate JavaScript

---
+++ QuizBytesSolution/QuizBytesWebsite/wwwroot/js/ChallengeViewLogic.js
@@ -0,0 +1,21 @@
+﻿//0.Button must act based on 'var registered' (if is true then it will deregister and if is false will move on to next step)
+//1. Button must check if there are available spots on click
+//2. Button must either register or spit out an error based on result
+//3. button must change text
+var registered = false; // this will be replaced by another variable to handle retrieving data from db(indirectly)
+var availableSpaces;
+$("#register-button-logic").ready(
+function changeButtonLogicToHandleRegisterAndDeregister() {
+    if (registered==false && availableSpaces>0) {
+        document.getElementById("register-button-logic").innerHTML = "FOR THE HORDE";
+        //calls method that adds user from challenge table
+    }
+    if (registered) {
+        document.getElementById("register-button-logic").innerHTML = "Deserter!";
+        //calls method that removes user from challenge table
+    }
+    else {
+        document.getElementById("error-message").innerHTML = "Sorry, the challenge has already started or there are no available spaces!";
+    }
+
+})

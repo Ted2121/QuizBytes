0f4bebd644dcf3a332f4cfa9d320c6ca67bd085f
Ted <andrei.teodor21@yahoo.ro>
2022-11-16 22:10:33 +0100

    minor change

---
+++ QuizBytesSolution/QuizBytesAPIServer/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessDefinitionLibrary.DAO_Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using QuizBytesAPIServer.DTOs;
+
+namespace QuizBytesAPIServer.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class LoginController : ControllerBase
+{
+    IWebUserDataAccess WebUserDataAccess { get; set; }
+    public LoginController(IWebUserDataAccess webUserDataAccess)
+    {
+        WebUserDataAccess = webUserDataAccess;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> Post([FromBody] WebUserDto webUser) =>
+            await WebUserDataAccess.LoginAsync(webUser.Username, webUser.Password);
+}
---
+++ QuizBytesSolution/QuizBytesAPIServer/Controllers/WebUserController.cs
@@ -0,0 +1,73 @@
+﻿using DataAccessDefinitionLibrary.DAO_Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using QuizBytesAPIServer.DTOs;
+using QuizBytesAPIServer.DTOs.Converters;
+using SQLAccessImplementationLibrary;
+
+namespace QuizBytesAPIServer.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class WebUserController : ControllerBase
+{
+    public IWebUserDataAccess WebUserDataAccess { get; set; }
+
+    public WebUserController(IWebUserDataAccess webUserDataAccess)
+    {
+        WebUserDataAccess = webUserDataAccess;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<WebUserDto>>> GetAllWebUsersAsync()
+    {
+        var webUsers = await WebUserDataAccess.GetAllWebUsersAsync();
+
+        if(webUsers == null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            return Ok(webUsers.ToDtos());
+        }
+        
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> CreateWebUserAsync(WebUserDto webUser)
+    {
+        return Ok(await WebUserDataAccess.InsertWebUserAsync(webUser.FromDto()));
+    }
+
+    [HttpGet]
+    [Route("Username")]
+    public async Task<ActionResult<WebUserDto>> GetWebUserByUsernameAsync(string username)
+    {
+        var webUser = await WebUserDataAccess.GetWebUserByUsernameAsync(username);
+
+        if(webUser == null)
+        {
+            return NotFound();
+
+        }
+        else
+        {
+            return Ok(webUser.ToDto());
+        }
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateWebUserAsync(WebUserDto webUser)
+    {
+
+    }
+
+    [HttpPut]
+    [Route("password")]
+    public async Task<ActionResult> UpdatePasswordAsync(WebUserDto webUser)
+    {
+
+    }
+}
+
+

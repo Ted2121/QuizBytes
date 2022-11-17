66ed6031579ae449460cebe4454349c585bc66ac
Ted <andrei.teodor21@yahoo.ro>
2022-11-17 14:49:16 +0100

    added a configuration file that contains the URI of the API for testing purposes

---
+++ QuizBytesSolution/WebclientWebserverIntegrationTesting/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebclientWebserverIntegrationTesting;
+
+public static class Configuration
+{
+    private const string WEB_API_URI = "https://localhost:7215/api/v1/";
+}

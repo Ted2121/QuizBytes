82774365f4dd653785aea3c64634d0af0b8233c9
kir40lmao <93580277+kir40lmao@users.noreply.github.com>
2022-11-21 00:27:35 +0100

    QuizView sidebar added
    The side navbar is now existing + some other js + other shit logic is in there

---
+++ QuizBytesSolution/QuizBytesWebsite/wwwroot/js/QuizViewLogic.js
@@ -0,0 +1,21 @@
+﻿function expand() {
+    //needs to check which element was pressed and retrieve the required data(if Course was pressed then it would retrieve a
+    //list of courses and so on)
+    //needs to send user to the desired course/subject/ whatever
+    let submenu = document.getElementById("submenu"); 
+    submenu.innerHTML = 
+       `<a>Course1</a>
+        <a>Course2</a>
+        <a>Course3</a>
+        <a>Course4</a>
+        <a>Course5</a>
+        <a>Course6</a>
+        <a>Course7</a>
+        <a>Course8</a>
+        <a>Course9</a>
+        <a>Course10</a>
+        <a>Course11</a>
+        <a>Course12</a>`
+    let opened = true;
+    //if button is pressed again it should close the menu
+}

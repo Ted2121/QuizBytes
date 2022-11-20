15a2a580860a4ffa27cdac4bb7fd8f55525096b4
kir40lmao <93580277+kir40lmao@users.noreply.github.com>
2022-11-21 00:37:09 +0100

    Update QuizViewLogic.js
    comments added for help later on

--- QuizBytesSolution/QuizBytesWebsite/wwwroot/js/QuizViewLogic.js
+++ QuizBytesSolution/QuizBytesWebsite/wwwroot/js/QuizViewLogic.js
@@ -1,9 +1,12 @@
 ﻿function expand() {
+    //Maybe it would work if I add a function which checks if the button that was clicked has the .active css class
+    //and if yes removes these items and if not adds them to the #submenu a .active
     //needs to check which element was pressed and retrieve the required data(if Course was pressed then it would retrieve a
     //list of courses and so on)
     //needs to send user to the desired course/subject/ whatever
     let submenu = document.getElementById("submenu"); 
-    submenu.innerHTML = 
+    submenu.innerHTML = //this will go through a for loop to add an <a></a> for each element in an array which will come from
+        //lower layers. The rest will be handles can hopefully be handled in C#
        `<a>Course1</a>
         <a>Course2</a>
         <a>Course3</a>

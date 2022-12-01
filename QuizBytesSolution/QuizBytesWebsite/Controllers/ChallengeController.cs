c5e7fc79a083306dbe16f01b5af52fe24193cb2a
Ted <andrei.teodor21@yahoo.ro>
2022-12-01 13:15:33 +0100

    removed some stuff that was extra

--- QuizBytesSolution/QuizBytesWebsite/Controllers/ChallengeController.cs
+++ QuizBytesSolution/QuizBytesWebsite/Controllers/ChallengeController.cs
@@ -130,49 +130,7 @@ public class ChallengeController : Controller
             Course = courseForChallenge
         });
     }
-    public async Task<JsonResult> Leaderboard()
-    {
-
-        var leaderboard = LeaderboardBuilder.BuildLeaderboardFromParticipantList();
-        var leaderboardInfo = FilterUserProperties(leaderboard);
-        return Json(leaderboardInfo);
-    }
-
-    private List<LeaderboardInfo> FilterUserProperties(LeaderboardDto leaderboard)
-    {
-        var leaderboardWithFilteredProperties = new List<LeaderboardInfo>();
-        var users = leaderboard.Leaderboard;
-
-        if (users.Any())
-        {
-            for (int i = 0; i < users.Count; i++)
-            {
-                var leaderboardInfo = new LeaderboardInfo()
-                {
-                    Username = users[i].Username,
-                    Points = CalculateUserPoints(users[i].CorrectAnswers),
-                    ElapsedTime = ElapsedSecondsToString(users[i].ElapsedSecondsInChallenge)
-                };
-
-                leaderboardWithFilteredProperties.Add(leaderboardInfo);
-            }
-        }
-        return leaderboardWithFilteredProperties;
-    }
-
-    private int CalculateUserPoints(int numberOfCorrectAnswers) => numberOfCorrectAnswers * 8;
-    private string ElapsedSecondsToString(int elapsedSeconds)
-    {
-        var timespan = TimeSpan.FromSeconds(elapsedSeconds);
-        return timespan.ToString(@"hh\:mm\:ss");
-    }
-
-    internal class LeaderboardInfo
-    {
-        public string? Username { get; set; }
-        public int Points { get; set; }
-        public string? ElapsedTime { get; set; }
-    }
+    
     #endregion
 
 }
